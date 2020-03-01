1cdb0958643f64dcc5b5d2800d61e4c3710c286d
Marcin Cholewka <marcin.cholewka12@gmail.com>
2020-03-01 10:22:46 +0100

    Replace sample controller with empty CurrenciesController, install MediatR package

---
+++ Currencies.Api/Controllers/CurrenciesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Currencies.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrenciesController : ControllerBase
+    {
+        private readonly ILogger<CurrenciesController> _logger;
+
+        public CurrenciesController(ILogger<CurrenciesController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
